b34905636c77720b8be0ec1632175c472160a678
Rana Talukdar <147577902+Auth0r-C0dez@users.noreply.github.com>
2025-04-04 19:24:11 +0530

    Add files via upload

---
+++ NODEjs/file.js
@@ -0,0 +1,5 @@
+const join = require("./cal.js")
+
+// also can be done as const {add,sub} = require("./cal.js") then we dont need to make the join variable we can just simply use the function name.
+
+console.log(join.sub(2,3));

9661a58b109b67ca914653660baf1eaa79e44280
Rana Talukdar <147577902+Auth0r-C0dez@users.noreply.github.com>
2025-09-07 21:22:10 +0530

    Create app.js

---
+++ ExpressJS/Server-middleware/app.js
@@ -0,0 +1,171 @@
+const express = require("express");
+const fs = require("fs");
+const morgan = require("morgan");
+
+const app = express();
+
+//middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.use((req,res,next) => {
+    console.log("middle ware is created ");
+    next();
+});
+
+app.use((req,res,next) => {
+    req.requestTime = new Date().toISOString();
+    next();
+});
+
+// app.get('/', (req,res) => {
+//     res.status(200).json({message :"hello from the server",
+//         app:"the app" }
+//     );
+// });
+
+//routes
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+const getAllTours = (req,res) => {
+    res.status(200).json({
+        status:'success',
+        requestedAt : req.requestTime,
+        results : tours.length,
+        data : {
+            tours
+        }
+    });
+}
+
+const getTour = (req,res) => {
+    console.log(req.params) ; //the id or the variable that we will be sending is the parameter
+    const id = req.params.id*1;
+    const tour = tours.find(el => el.id === id);
+
+    if(!tour) {
+        return res.status(404).json({
+            status:'fail',
+            message:'invalid id'
+        });
+    }
+
+    
+
+    res.status(200).json ({
+        status :'success',
+        data : {
+            tour
+        }
+        
+    });
+}
+const createTour = (req,res) => {
+    //console.log(req.body);
+    const newId = tours[tours.length -1].id +1;
+    const newTour = Object.assign({id: newId},req.body);
+    tours.push(newTour);
+
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`,JSON.stringify(tours),err => {
+        res.status(201).json({
+            status:'success',
+            data: {
+                tour:newTour
+            }
+        })
+    })
+    //res.send('done');
+}
+const updateTour = (req,res) => {
+     if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status:'fail',
+            message:'invalid id'
+        });
+    }
+
+    res.status(200).json({
+        status:'success',
+        data :{
+            tour : '<uodated tour here ></uodated>'
+        }
+    })
+}
+const deleteTour = (req,res) => {
+     if(req.params.id * 1 > tours.length) {
+        return res.status(404).json({
+            status:'fail',
+            message:'invalid id'
+        });
+    }
+
+    res.status(204).json({
+        status:'success',
+        data :null
+    });
+}
+
+const getAllUsers = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+const getUser = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+const createUser = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+const updateUser = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+const deleteUser = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+// app.get('/api/v1/tours',getAllTours);
+// app.get('/api/v1/tours/:id' , getTour);
+// app.post('/api/v1/tours',createTour);
+// app.patch('/api/v1/tours/:id',updateTour);
+// app.delete('/api/v1/tours/:id',deleteTour);
+
+//api endpoints
+app.route('/api/v1/tour')
+  .get(getAllTours)
+  .post(createTour);
+
+app.route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
+
+app.route('/api/v1/users')
+  .get(getAllUsers)
+  .post(createUser);
+  
+
+app.route('/api/v1/users/:id') 
+  .get(getUser)
+  .patch(updateUser)
+  .delete(deleteUser);
+
+const port = 3000;
+app.listen(port,() => {
+    console.log("app is running in port");
+});

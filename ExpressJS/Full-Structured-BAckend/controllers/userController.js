a217f32a0b76bde2e6659e87dd6e65267dd2e8f4
Rana Talukdar <147577902+Auth0r-C0dez@users.noreply.github.com>
2025-10-04 01:55:41 +0530

    Create userController.js

---
+++ ExpressJS/Full-Structured-BAckend/controllers/userController.js
@@ -0,0 +1,36 @@
+
+
+exports.getAllUsers = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+exports.getUser = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+exports.createUser = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+exports.updateUser = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
+
+exports.deleteUser = (req,res) => {
+    res.status(500).json ({
+        status:'error',
+        message:'this route is not yet set'
+    });
+};
